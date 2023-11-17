c41b994c7b8ffcec23b3109eb6918d009c4244a2
unknown <allanbarbosa123@hotmail.com>
2023-11-17 11:49:36 -0300

    exemplos da aula 1

---
+++ tipo-booleano.js
@@ -0,0 +1,18 @@
+// true = verdadeiro
+const primeiroNumero = 5;
+const segundoNumero= 5;
+
+// console.log(primeiroNumero === segundoNumero);
+
+// false = falso
+const primeiroNumero1 = 5;
+const segundoNumero2 = 10;
+
+// console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "cu";
+const texto2 = "cu";
+
+console.log(texto1 === texto2);
+
+
---
+++ tipo-string.js
@@ -0,0 +1,11 @@
+const texto1 = "olá, mundo";
+const texto2 = 'olá, mundo';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "31846";
+
+const citacao = "Meu nome é ";
+const meuNome = "Allan";
+
+//concatenação (+)
+
+console.log(citacao + meuNome);
