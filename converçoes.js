ef277ede28a7a802423f64a0cb4867e983e63a56
unknown <allanbarbosa123@hotmail.com>
2023-11-22 11:31:20 -0300

    codigos  var e converções

---
+++ converçoes.js
@@ -0,0 +1,18 @@
+// tipo dado
+// booleanos
+
+//conversão implicita
+const numero = 69;
+const numeroString = "69";
+console.log(numero == numeroString);
+
+//Number()
+//String()
+
+//converção explicita
+console.log(numero + Number(numeroString));
+
+
+
+
+
